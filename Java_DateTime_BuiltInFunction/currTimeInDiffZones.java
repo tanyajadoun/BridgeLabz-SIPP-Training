878f4ab0d0203459dbfac0ca1e6342e1726ee457
Tanya Singh Jadoun <tanyasingh1932@gmail.com>
2025-06-19 16:09:07 +0530

    [Tanya Singh Jadoun] Added DateTime and BuiltIn Functions Practice Questions

---
+++ Java_DateTime_BuiltInFunction/currTimeInDiffZones.java
@@ -0,0 +1,22 @@
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class currTime {
+    public static void main(String[] args) {
+        ZoneId gmtZone = ZoneId.of("GMT");
+        ZoneId istZone = ZoneId.of("Asia/Kolkata");
+        ZoneId pstZone = ZoneId.of("America/Los_Angeles");
+
+        ZonedDateTime gmtTime = ZonedDateTime.now(gmtZone);
+        ZonedDateTime istTime = ZonedDateTime.now(istZone);
+        ZonedDateTime pstTime = ZonedDateTime.now(pstZone);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss z");
+
+        System.out.println("current time in gmt is " + gmtTime.format(formatter));
+        System.out.println("current time in ist is " + istTime.format(formatter));
+        System.out.println("current time in pst is " + pstTime.format(formatter));
+
+    }
+}
